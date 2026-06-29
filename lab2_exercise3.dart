c75b15560dee077beb2dc46427b2abf731f7c157
yeelingc67 <yeelingc67@nu.ac.th>
2026-06-30 02:38:49 +0700

    update lab2_exercise3

--- lab2_exercise3.dart
+++ lab2_exercise3.dart
@@ -1,3 +1,18 @@
 main(){
-    print('test');
+  List<String> subjects = ['Math', 'Moblie', 'Algorithm'];
+  print('Number subjects: ${subjects.length}');
+  print('First subject: ${subjects[0]}');
+  print('Last subject: ${subjects.last}');
+  subjects.add('Digital');
+  print('All subjects: $subjects');
+
+  print("");
+
+  Map<String, int> studentScores = {'Math': 85,'Moblie': 98,};
+  print('Score Moblie: ${studentScores['Math']}');
+
+  studentScores['Algorithm'] = 85;
+  print('Updated scores: $studentScores');
+  print('All subjects : ${studentScores.keys}');
+  print('All scores : ${studentScores.values}');
 }
